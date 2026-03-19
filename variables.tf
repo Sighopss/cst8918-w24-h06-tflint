64b99c203ec377aceb07cb5185d8f89c2fbfb5b7
Trevorkutto <120150007+Trevorkutto@users.noreply.github.com>
2026-03-18 23:47:16 -0400

    Add TFLint configuration and fix all linting issues

---
+++ variables.tf
@@ -0,0 +1,17 @@
+# Define config variables
+variable "label_prefix" {
+  type        = string
+  description = "Your college username. This will form the beginning of various resource names."
+}
+
+variable "region" {
+  type        = string
+  default     = "westus3"
+  description = "The Azure region for resource deployment."
+}
+
+variable "admin_username" {
+  type        = string
+  default     = "azureadmin"
+  description = "The username for the local user account on the VM."
+}
